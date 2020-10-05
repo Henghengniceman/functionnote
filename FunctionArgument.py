52ee27572b09205f6145295fefb0e96297a1dce8
Henghengniceman <71984922+Henghengniceman@users.noreply.github.com>
2020-10-05 17:13:43 +0200

    Create FunctionArgument.py

---
+++ FunctionArgument.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 17:12:18 2020
+
+@author: Hengheng Zhang
+
+E-Mail: hengheng.zhang@kit.edu
+
+Function：
+
+"""
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    for kw in keywords:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
